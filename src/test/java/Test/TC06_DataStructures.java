288d556b34ac1026c92096a9bd48f5a508923179
sumathiselvaraj <97060696+sumathiselvaraj@users.noreply.github.com>
2026-03-04 19:12:49 -0700

    Reviewed Ds Module

--- src/test/java/Test/TC06_DataStructures.java
+++ src/test/java/Test/TC06_DataStructures.java
@@ -1,92 +1,115 @@
 package Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.List;
 
 import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.ITestAnnotation;
 import org.testng.annotations.Test;
 
 import TestPackage.DataStrData;
 import utils.ElementUtil;
 
-@Test(groups = { "Get Started", "Sign in" })
+@Test(groups = {"Get Started", "Sign in"})
 public class TC06_DataStructures extends Hooks {
 
-	@Test(priority = 1)
-	public void navigateToDataStructuresPage() {
-		pom.getHomePage().clickGetStarted("Data Structures-Introduction");
-		logger.debug("Current page title: {}", ElementUtil.getTitle());
-		Assert.assertEquals(ElementUtil.getTitle(), "Data Structures-Introduction",
-				"Failed to navigate to Data Structures page");
-
-	}
-
-	@Test(priority = 2, dataProvider = "StaticContent", dataProviderClass = DataStrData.class)
-	public void verifyStaticContent(String expectedText) {
-		List<String> headings = pom.getDataStructurePage().getheadingtext();
-		System.out.println("Headings: " + headings);
-		logger.info("Headings: {}", headings);
-		boolean found = false;
-
-		for (String heading : headings) {
-			if (heading.equalsIgnoreCase(expectedText)) {
-				found = true;
-				break;
-			}
-		}
-		Assert.assertTrue(found, "Expected text '" + expectedText + "' not found in the headings: " + headings);
-	}
-
-	@Test(priority = 3)
-	public void navigateToTimeComplexityPage() {
-		pom.getDataStructurePage().clickTopicLink("Time Complexity");
-		System.out.println("Current URL: " + ElementUtil.getURL());
-		logger.debug("Current page title: {}", ElementUtil.getTitle());
-		String pageurltext = "Data Structures Introduction";
-		String expected = pageurltext.toLowerCase().replace(" ", "-");
-		Assert.assertTrue(ElementUtil.getURL().contains(expected),
-				"URL does not contain expected text: " + pageurltext);
-		logger.info("User is on the expected page: " + pageurltext);
-
-	}
-
-	@Test(priority = 4)
-	public void verifyTryHereButtonVisibility() {
-
-		logger.info("Checking Try Here button visibility on Time Complexity page");
-		boolean isDisplayed = pom.getDataStructurePage().checktryherebutton_displayed();
-		if (!isDisplayed) {
-			logger.error("Try Here button is not visible on Time Complexity page");
-		}
-		{
-			Assert.assertTrue(isDisplayed, "Try Here button is not visible on Time Complexity page");
-			logger.info("Verified Try Here button is visible on Time Complexity page");
-		}
-	}
-
-	@Test(priority = 5)
-	public void navigateToTryEditorPage() {
-		pom.getDataStructurePage().clickTryHereButton();
-		Assert.assertTrue(ElementUtil.getURL().contains("tryEditor"), "user is not on tryeditor screen");
-		logger.info("User is on the Try Editor page after clicking Try Here button");
-	}
-
-	@Test(priority = 6)
-	public void getErrorMessageOnRunningEmptyCode() {
-		pom.getDataStructurePage().clickrunBtn();
-		String expectedErrorMsg = "Code Editor is empty. Please enter your code.";
-		String actualErrorMsg = pom.getDataStructurePage().getErrAlert();
-		Assert.assertEquals(actualErrorMsg, expectedErrorMsg,
-				"Expected error alert not found. Actual message: " + pom.getDataStructurePage().getErrAlert());
-		logger.info("Error message displayed: " + pom.getDataStructurePage().getErrAlert());
+    @BeforeMethod
+    public void clickDSButton(Method method) {
+        logger.info("Clicking Data Structures-Introduction link in home page");
+        pom.getHomePage().clickGetStarted("Data Structures-Introduction");
+		Test testAnnotation = method.getAnnotation(Test.class);
+		List<String> groupList = Arrays.asList(testAnnotation.groups());
+		if (groupList.contains("Time Complexity")) {
+			pom.getDataStructurePage().clickTopicLink("Time Complexity");
 		}
 
-	@Test(priority = 7)
-	public void navigateToPracticeQuestionsLink() {
-		ElementUtil.navigateBack();
-		pom.getDataStructurePage().clickPracticeQuestionsLink();
-		List<String> questions = pom.getDataStructurePage().getQuestionsList();
-		Assert.assertTrue(!questions.isEmpty(),
-				"No questions are displayed in Practice Questions section of DataStructures module");
-	}
+
+    }
+
+    @Test(priority = 1)
+    public void navigateToDataStructuresPage() {
+
+        logger.debug("Current page title: {}", ElementUtil.getTitle());
+        Assert.assertEquals(ElementUtil.getTitle(), "Data Structures-Introduction",
+                "Failed to navigate to Data Structures page");
+
+    }
+
+    @Test(priority = 2, dataProvider = "StaticContent", dataProviderClass = DataStrData.class)
+    public void verifyStaticContent(String expectedText) {
+        List<String> headings = pom.getDataStructurePage().getheadingtext();
+        System.out.println("Headings: " + headings);
+        logger.info("Headings: {}", headings);
+        boolean found = false;
+
+        for (String heading : headings) {
+            if (heading.equalsIgnoreCase(expectedText)) {
+                found = true;
+                break;
+            }
+        }
+        Assert.assertTrue(found, "Expected text '" + expectedText + "' not found in the headings: " + headings);
+    }
+
+    @Test(priority = 3, groups = "Time Complexity")
+    public void navigateToTimeComplexityPage() {
+
+        logger.info("Current page title: {}", ElementUtil.getTitle());
+        String pageUrlText = "Data Structures Introduction";
+        String expected = pageUrlText.toLowerCase().replace(" ", "-");
+        logger.info("User is on the expected page: {}", pageUrlText);
+        Assert.assertTrue(ElementUtil.getURL().contains(expected),
+                "URL does not contain expected text: " + pageUrlText);
+
+
+    }
+
+    @Test(priority = 4, groups = "Time Complexity")
+    public void verifyTryHereButtonVisibility() {
+
+        logger.info("Checking Try Here button visibility on Time Complexity page");
+        boolean isDisplayed = pom.getDataStructurePage().checktryherebutton_displayed();
+        if (!isDisplayed) {
+            logger.error("Try Here button is not visible on Time Complexity page");
+        }
+        {
+            Assert.assertTrue(isDisplayed, "Try Here button is not visible on Time Complexity page");
+            logger.info("Verified Try Here button is visible on Time Complexity page");
+        }
+    }
+
+    @Test(priority = 5, groups = "Time Complexity")
+    public void navigateToTryEditorPage() {
+
+        pom.getDataStructurePage().clickTryHereButton();
+        Assert.assertTrue(ElementUtil.getURL().contains("tryEditor"), "user is not on tryeditor screen");
+        logger.info("User is on the Try Editor page after clicking Try Here button");
+    }
+
+    @Test(priority = 6, groups = "Time Complexity")
+    public void navigateToPracticeQuestionsLink() {
+
+        pom.getDataStructurePage().clickPracticeQuestionsLink();
+        List<String> questions = pom.getDataStructurePage().getQuestionsList();
+        Assert.assertFalse(questions.isEmpty(),
+                "No questions are displayed in Practice Questions section of DataStructures module");
+    }
+
+    @AfterMethod
+    public void backToHome(ITestResult result) {
+        String[] groups = result.getMethod().getGroups();
+        List<String> groupList = Arrays.asList(groups);
+
+        if (groupList.contains("Time Complexity")) {
+            while (!ElementUtil.getURL().contains("home")) {
+                ElementUtil.navigateBack();
+            }
+        }
+    }
+
 
 }
